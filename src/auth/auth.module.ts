30dcf91c70be40d5ca049ad2239d1fc70d4bb4fb
Roman <roman.batiuk87@gmail.com>
2022-03-06 17:33:12 +0200

    authentication with passport-jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose-next';
 import { AuthController } from './auth.controller';
 import { UserModel } from './user.model';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJWTCongig } from 'src/configs/jwt.config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
 	imports: [
@@ -13,7 +19,15 @@ import { UserModel } from './user.model';
 				},
 			},
 		]),
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJWTCongig,
+		}),
+		PassportModule,
+		ConfigModule,
 	],
 	controllers: [AuthController],
+	providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
