8a6b2cfbd9ad78a372ee6219c1a8d747433b776d
Roman <roman.batiuk87@gmail.com>
2022-03-07 08:54:54 +0200

    add custom pipe for validation id

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -12,6 +12,7 @@ import {
 	UsePipes,
 	ValidationPipe,
 } from '@nestjs/common';
+import { IdValidationPipe } from 'src/pipes/id-validation.pipe';
 import { CreateProductDto } from './dto/create-product.dto';
 import { FindProductDto } from './dto/find-product.dto';
 import { PRODUCT_NOT_FOUND_ERROR } from './product.constants';
@@ -27,7 +28,7 @@ export class ProductController {
 	}
 
 	@Get(':id')
-	async get(@Param('id') id: string) {
+	async get(@Param('id', IdValidationPipe) id: string) {
 		const product = await this.productService.findById(id);
 
 		if (!product) {
@@ -38,7 +39,7 @@ export class ProductController {
 	}
 
 	@Delete(':id')
-	async delete(@Param('id') id: string) {
+	async delete(@Param('id', IdValidationPipe) id: string) {
 		const deletedProduct = await this.productService.deleteById(id);
 
 		if (!deletedProduct) {
@@ -47,7 +48,7 @@ export class ProductController {
 	}
 
 	@Patch(':id')
-	async patch(@Param('id') id: string, @Body() dto: CreateProductDto) {
+	async patch(@Param('id', IdValidationPipe) id: string, @Body() dto: CreateProductDto) {
 		const updatedProduct = await this.productService.updateById(id, dto);
 
 		if (!updatedProduct) {
--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -12,6 +12,7 @@ import {
 	ValidationPipe,
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { IdValidationPipe } from 'src/pipes/id-validation.pipe';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND_ERROR } from './review.constants';
 import { ReviewService } from './review.service';
@@ -29,7 +30,7 @@ export class ReviewController {
 
 	@UseGuards(JwtAuthGuard)
 	@Delete(':id')
-	async delete(@Param('id') id: string) {
+	async delete(@Param('id', IdValidationPipe) id: string) {
 		const deletedDoc = await this.reviewService.delete(id);
 		if (!deletedDoc) {
 			throw new HttpException(REVIEW_NOT_FOUND_ERROR, HttpStatus.NOT_FOUND);
@@ -38,7 +39,10 @@ export class ReviewController {
 
 	@UseGuards(JwtAuthGuard)
 	@Get('buyProduct/:productId')
-	async getByProduct(@Param('productId') productId: string, @UserEmail() email: string) {
+	async getByProduct(
+		@Param('productId', IdValidationPipe) productId: string,
+		@UserEmail() email: string,
+	) {
 		return this.reviewService.findByProductId(productId);
 	}
 }
