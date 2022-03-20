7fea567244511ab368efc46fd1b40341e90bd772
romanbatiuk <roman.batiuk87@gmail.com>
2022-03-20 16:13:03 +0200

    fixed mongo.config

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -4,31 +4,21 @@ import { TypegooseModuleOptions } from 'nestjs-typegoose-next';
 export const getMongoConfig = async (
 	configService: ConfigService,
 ): Promise<TypegooseModuleOptions> => {
-	// console.log(getMongoString(configService));
-
 	return {
 		uri: getMongoString(configService),
+		...getMongoOptions(),
 	};
 };
 
-// mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]
-// mongodb://admin:admin@localhost:27017/mytop
-
 const getMongoString = (configService: ConfigService) =>
-	// 'mongodb+srv://' +
-	// configService.get('MONGO_LOGIN') +
-	// ':' +
-	// configService.get('MONGO_PASSWORD') +
-	// '@' +
-	// configService.get('MONGO_HOST') +
-	// '/' +
-	// configService.get('MONGO_AUTH_DATABASE');
 	'mongodb://' +
 	configService.get('MONGO_LOGIN') +
 	':' +
 	configService.get('MONGO_PASSWORD') +
 	'@' +
 	configService.get('MONGO_HOST') +
+	':' +
+	configService.get('MONGO_PORT') +
 	'/' +
 	configService.get('MONGO_AUTH_DATABASE');
 
